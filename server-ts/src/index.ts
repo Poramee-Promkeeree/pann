bfde73963d18f1565d13f38ae1025d257cc9d245
psu6510110267 <6510110267@email.psu.ac.th>
2023-01-18 22:32:54 +0700

    เพิ่มMethod การใช้งานเพิ่มเติม

--- server-ts/src/index.ts
+++ server-ts/src/index.ts
@@ -3,11 +3,13 @@ import json from 'koa-json'
 import apiRouter from './api'
 import appConfig from './config'
 import loadFixtures from './fixture'
+import { koaBody } from 'koa-body'
 
 const app = new Koa()
 
 app.use(json())
 app.use(apiRouter.routes())
+app.use(koaBody())
 
 app.listen(8000)
 
