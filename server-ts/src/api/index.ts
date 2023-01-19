dfe2443bb6513f61417008ba74d987c3c58b5cac
psu6510110267 <6510110267@email.psu.ac.th>
2023-01-19 16:33:39 +0700

    เพิ่มการ auth ก่อนเข้า api

--- server-ts/src/api/index.ts
+++ server-ts/src/api/index.ts
@@ -1,15 +1,13 @@
+import { authMiddleware } from '../auth'
 import Router from 'koa-router'
 import announcement from './announcement'
 import userResult from './user_result'
 
 const apiRouter = new Router()
 
-apiRouter.get('/api/greet', (ctx,next) => {
-    ctx.body = {msg: 'Hello world'}
-})
+apiRouter.use('/api/announcement',authMiddleware,announcement.routes())
+apiRouter.use('/api/userResult',authMiddleware,userResult.routes())
 
-apiRouter.use('/api/announcement',announcement.routes())
-apiRouter.use('/api/userResult',userResult.routes())
 
 
 export default apiRouter
