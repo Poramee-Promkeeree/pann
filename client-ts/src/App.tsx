6e0260f0a02414dc0d719a9d583a42ea5ed26624
psu6510110267 <6510110267@email.psu.ac.th>
2023-01-20 17:19:27 +0700

    ทดสอบโดยMockข้อมูล announment และ UserReult

--- client-ts/src/App.tsx
+++ client-ts/src/App.tsx
@@ -1,25 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Repo from './repositories'
+import { useEffect ,useState } from 'react';
+import Announcement from './models/Announcement';
+import UserResult from './models/UserResult';
 
 function App() {
+  const [announcementList, setAnnouncementList] = useState<Announcement[]>([])
+  const [userResultList, setuserResultList] = useState<UserResult[]>([])
+  const fetchannouncementList = async () => {
+    const result = await Repo.announcements.getAll()
+    if(result){
+      setAnnouncementList(result)
+    }
+  }
+  const fetchuserResultList = async () => {
+    const result = await Repo.userResult.getAll()
+    if(result){
+      setuserResultList(result)
+    }
+  }
+
+  useEffect(() => {
+    fetchannouncementList()
+    fetchuserResultList()
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {announcementList.map(announcement => (
+        <div key={announcement.id}>
+          <p>ID : {announcement.id}</p>
+          <p>Description : {announcement.description}</p>
+          <p>Positive : {announcement.remarkIfPositive}</p>
+          <p>Negative : {announcement.remarkIfNegative}</p>
+          <p>Time : {announcement.pubDateTime.toLocaleString('en-US', {
+              year: 'numeric',
+              month: '2-digit',
+              day: '2-digit',
+              hour: '2-digit',
+              minute: '2-digit',
+              second: '2-digit'
+            })}</p>
+          <p>UserCode : {announcement.userCode}</p>
+        </div>
+      ))}
+      <hr />
+      <div>
+        {userResultList.map(userResult => (
+          <div key={userResult.id}>
+            <p>ID : {userResult.id}</p>
+            <p>Topic : {userResult.announcement.topic}</p>
+            <p>Result : {userResult.result}</p>
+            <p>UserCode : {userResult.userCode}</p>
+          </div>
+        ))}
+      </div>
     </div>
+   
   );
 }
 
