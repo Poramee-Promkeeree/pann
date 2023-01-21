6b229252d916091d9540f4239dde8a104936d7e5
psu6510110267 <6510110267@email.psu.ac.th>
2023-01-21 19:44:08 +0700

    เพิ่ม routes announcement

--- client-ts/src/config/routes.tsx
+++ client-ts/src/config/routes.tsx
@@ -3,6 +3,7 @@ import { useAppCtx } from "../AppProvider"
 
 import Login from "../page/login"
 import UserResultList from "../page/user-result-list"
+import AnnouncementList from "../page/announcement-list"
 
 type Props = {
     staffOnly?: boolean
@@ -30,6 +31,7 @@ const AppRoutes = () => {
             <Route index element={<Login />} />
             <Route path="login" element={<Login />}/>
             <Route path="home" element={<ProtectedRoute><UserResultList/></ProtectedRoute>} />
+            <Route path='announcement' element={<ProtectedRoute><AnnouncementList/></ProtectedRoute>}/>
         </Routes>
     )
 }
