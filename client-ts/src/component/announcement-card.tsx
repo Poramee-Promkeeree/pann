57bd513b1319627008beacfae74b111554c68377
Poramee Promkeeree <poramee0013@gmail.com>
2023-01-30 22:40:48 +0700

    แก้การปรับแต่ง

--- client-ts/src/component/announcement-card.tsx
+++ client-ts/src/component/announcement-card.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Button, Card, CardActionArea, CardActions, CardContent, CardHeader, Dialog, DialogTitle, Grid, IconButton, Tab, Table, TableBody, TableCell, TableHead, TableRow, Tabs, Typography } from "@mui/material";
+import { Button, Card, CardActionArea, CardActions, CardContent, CardHeader, Dialog, DialogTitle, Grid, IconButton, Tab, Table, TableBody, TableCell, TableHead, TableRow, Tabs, Typography ,Paper} from "@mui/material";
 import { Box } from "@mui/system";
 import { Close, Delete, Edit, Upload } from "@mui/icons-material";
 import { read, utils } from 'xlsx';
@@ -123,76 +123,78 @@ function AnnouncementCard(props: Prop) {
 
 
   return (
-    <Box>
-      <Card sx={{ maxWidth: 500, height: 240 }}>
-        <CardHeader
-          sx={{ height: '30%' }}
-          title={announcement?.topic}
-          subheader={new Date(announcement?.pubDateTime?.toString()).toLocaleString('en-GB')}
-          action={
-            <IconButton sx={{ '&:hover': { color: 'red' } }} onClick={onDelete}>
-              <Delete />
-            </IconButton>
-          }
-        />
-        <CardActionArea sx={{ height: '56%' }} onClick={() => setPopup(true)}>
-          <CardContent sx={{ height: '40%' }}>
-            <Grid container spacing={2} columns={5}>
-              <Grid item xs={3}>
-                <Typography component="div">
-                  {announcement.description}
-                </Typography>
+    <Paper sx={{ bgcolor:"#FE0000"}}>
+      <Box>
+        <Card sx={{ maxWidth: 500, height: 240 }}>
+          <CardHeader
+            sx={{ height: '30%' }}
+            title={announcement?.topic}
+            subheader={new Date(announcement?.pubDateTime?.toString()).toLocaleString('en-GB')}
+            action={
+              <IconButton sx={{ '&:hover': { color: 'red' } }} onClick={onDelete}>
+                <Delete />
+              </IconButton>
+            }
+          />
+          <CardActionArea sx={{ height: '56%' }} onClick={() => setPopup(true)}>
+            <CardContent sx={{ height: '40%' }}>
+              <Grid container spacing={2} columns={5}>
+                <Grid item xs={3}>
+                  <Typography component="div">
+                    {announcement.description}
+                  </Typography>
+                </Grid>
               </Grid>
-            </Grid>
-          </CardContent>
-          <CardActions sx={{ justifyContent: 'flex-end' }}>
-            <Edit color="primary" />
-          </CardActions>
-        </CardActionArea>
-      </Card>
+            </CardContent>
+            <CardActions sx={{ justifyContent: 'flex-end' }}>
+              <Edit color="primary" />
+            </CardActions>
+          </CardActionArea>
+        </Card>
 
-      <Dialog PaperProps={{ sx: { minWidth: "50%", height: "55%" } }} open={popup} onClose={() => setPopup(false)}>
-        <DialogTitle sx={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Tabs value={tabIndex} onChange={(event: React.SyntheticEvent, newValue: number) => setTabIndex(newValue)} aria-label="basic tabs example">
-            <Tab label="General" />
-            <Tab label="Results List" />
-          </Tabs>
-          <IconButton onClick={() => setPopup(false)}>
-            <Close />
-          </IconButton>
-        </DialogTitle>
-        <Box hidden={tabIndex !== 0}>
-          <AnnouncementForm announcement={announcement} callbackFn={onUpdate}></AnnouncementForm>
-        </Box>
-        <Box hidden={tabIndex !== 1}>
-          <Button variant="contained" component="label" sx={{ mx: 2 }}>
-            <Upload />
-            Import
-            <input hidden type="file" accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel" onChange={handleImport} />
-          </Button>
-          <Table>
-            <TableHead>
-              <TableRow>
-                {xlsxHeading.map((it, index) => <TableCell key={index}><b>{it}</b></TableCell>)}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {userResultList.map((userResult, index) =>
-                <TableRow key={index} sx={{ backgroundColor: getConditionalBgColor(userResult) }}>
-                  <TableCell>{userResult.userCode}</TableCell>
-                  <TableCell>{userResult.result}</TableCell>
-                  <TableCell>{userResult.resultType}</TableCell>
-                  <TableCell>{userResult.remark}</TableCell>
+        <Dialog PaperProps={{ sx: { minWidth: "50%", height: "55%" } }} open={popup} onClose={() => setPopup(false)}>
+          <DialogTitle sx={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Tabs value={tabIndex} onChange={(event: React.SyntheticEvent, newValue: number) => setTabIndex(newValue)} aria-label="basic tabs example">
+              <Tab label="General" />
+              <Tab label="Results List" />
+            </Tabs>
+            <IconButton onClick={() => setPopup(false)}>
+              <Close />
+            </IconButton>
+          </DialogTitle>
+          <Box hidden={tabIndex !== 0}>
+            <AnnouncementForm announcement={announcement} callbackFn={onUpdate}></AnnouncementForm>
+          </Box>
+          <Box hidden={tabIndex !== 1}>
+            <Button variant="contained" component="label" sx={{ mx: 2 }}>
+              <Upload />
+              Import
+              <input hidden type="file" accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel" onChange={handleImport} />
+            </Button>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  {xlsxHeading.map((it, index) => <TableCell key={index}><b>{it}</b></TableCell>)}
                 </TableRow>
-              )}
-            </TableBody>
-          </Table>
-          <Button disabled={!isImporting} variant="contained" component="label" sx={{ m: 2, float: 'right' }} onClick={handleSubmitImport}>
-            Submit
-          </Button>
-        </Box>
-      </Dialog>
-    </Box>
+              </TableHead>
+              <TableBody>
+                {userResultList.map((userResult, index) =>
+                  <TableRow key={index} sx={{ backgroundColor: getConditionalBgColor(userResult) }}>
+                    <TableCell>{userResult.userCode}</TableCell>
+                    <TableCell>{userResult.result}</TableCell>
+                    <TableCell>{userResult.resultType}</TableCell>
+                    <TableCell>{userResult.remark}</TableCell>
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+            <Button disabled={!isImporting} variant="contained" component="label" sx={{ m: 2, float: 'right' }} onClick={handleSubmitImport}>
+              Submit
+            </Button>
+          </Box>
+        </Dialog>
+      </Box>
+    </Paper>
   )
 
 }
